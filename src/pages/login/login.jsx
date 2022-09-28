aa0b66df8bc6155f212dd71be4a5cc76023ece12
Gabi Lemos <gabicarvalholemos@gmail.com>
2022-09-28 19:26:02 -0300

    created register page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,48 +1,137 @@
-import cover from '../../assets/img/cover-login.png';
-import { Grid, Box, TextField, Button, Stack } from '@mui/material';
-import LoginIcon from '@mui/icons-material/Login';
+import './login.css'
+import '../../assets/css/auth.css'
+
+import cover from '../../assets/img/cover-login.png'
+import logo from '../../assets/img/logo.png'
+
+import {
+  Grid,
+  Box,
+  TextField,
+  Button,
+  Stack,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+  FormControl,
+  InputLabel
+} from '@mui/material'
+
+import LoginIcon from '@mui/icons-material/Login'
+
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+import React, { useState } from 'react'
+
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return  <Grid container spacing={2} style={{height: '100vh'}}>
-            <Grid item sx={{
-              padding: '10px',
-              boxSizing: 'border-box',
-            }} xs={12} md={7} lg={8}>
-              <Stack spacing={2} style={{
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-              }}>
-              <img src={cover} style={{width: '100%'}} alt='Logo'/>
-              </Stack>
+  const [visibilityToggle, setVisibilityToggle] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisibilityToggle(!visibilityToggle)
+  }
+
+  return (
+    <Grid container spacing={2} style={{ height: '100vh' }}>
+      <Grid
+        className="leftSide"
+        item
+        sx={{
+          padding: '10px',
+          boxSizing: 'border-box'
+        }}
+        xs={12}
+        sm={7}
+      >
+        <Stack
+          spacing={2}
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingLeft: '20px',
+            paddingRight: '20px'
+          }}
+        >
+          <img src={cover} style={{ width: '100%' }} alt="Logo" />
+        </Stack>
+      </Grid>
+      <Grid item xs={12} sm={5}>
+        <Stack
+          spacing={2}
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingLeft: '20px',
+            paddingRight: '20px'
+          }}
+        >
+          <img src={logo} style={{ width: '30%' }} alt="Logo" />
+          <h2 className="loginTitle">Entre com seu usuário</h2>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="E-mail"
+                type="email"
+                variant="outlined"
+                margin="normal"
+                color="secondary"
+              />
             </Grid>
-            <Grid item xs={12} md={5} lg={4}>
-              <Stack spacing={2} style={{
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-              }}>
-                <h1>Entre com seu usuário</h1>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <TextField fullWidth id="outlined-basic" label="E-mail" type='email' variant="outlined" margin="normal" color="secondary"/>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField fullWidth id="outlined-basic" label="Senha" type='password' variant="outlined" margin="normal"  color="secondary"/>   
-                  </Grid>
-                  <Grid item xs={12}>
-                  <Button fullWidth variant="contained" color="secondary" endIcon={<LoginIcon />}>
-  Entrar
-</Button>
-                  </Grid>
-                </Grid>                              
-              </Stack>
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%', marginBottom: '10px' }}>
+                <InputLabel color="secondary">Senha</InputLabel>
+                <OutlinedInput
+                  color="secondary"
+                  fullWidth
+                  label="Senha"
+                  type={visibilityToggle ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="Clique para exibir"
+                        onClick={toggleVisibility}
+                      >
+                        {visibilityToggle ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  variant="outlined"
+                />
+              </FormControl>
             </Grid>
-          </Grid> 
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="secondary"
+                endIcon={<LoginIcon />}
+              >
+                Entrar
+              </Button>
+            </Grid>
+            <Link
+              style={{
+                textAlign: 'center',
+                display: 'block',
+                width: '100%',
+                marginTop: '5px',
+                color: '#121F4E'
+              }}
+              to="/register"
+            >
+              Criar conta
+            </Link>
+          </Grid>
+        </Stack>
+      </Grid>
+    </Grid>
+  )
 }
 
-export default Login;
+export default Login
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,5 +1,153 @@
+import '../../assets/css/auth.css'
+
+import cover from '../../assets/img/cover-register.png'
+import logo from '../../assets/img/logo.png'
+
+import {
+  Grid,
+  TextField,
+  Button,
+  Stack,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+  FormControl,
+  InputLabel
+} from '@mui/material'
+
+import LoginIcon from '@mui/icons-material/Login'
+
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+import React, { useState } from 'react'
+
+import { Link } from 'react-router-dom'
+
 const Register = () => {
-  return <h1>Registro</h1>
+  const [visibilityToggle, setVisibilityToggle] = useState(true)
+
+  const toggleVisibility = () => {
+    setVisibilityToggle(!visibilityToggle)
+  }
+
+  return (
+    <Grid container spacing={2} style={{ height: '100vh' }}>
+      <Grid
+        className="leftSide"
+        item
+        sx={{
+          padding: '10px',
+          boxSizing: 'border-box'
+        }}
+        xs={12}
+        sm={7}
+      >
+        <Stack
+          spacing={2}
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingLeft: '20px',
+            paddingRight: '20px'
+          }}
+        >
+          <img src={cover} style={{ width: '100%' }} alt="Logo" />
+        </Stack>
+      </Grid>
+      <Grid item xs={12} sm={5}>
+        <Stack
+          spacing={2}
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingLeft: '20px',
+            paddingRight: '20px'
+          }}
+        >
+          <img src={logo} style={{ width: '30%' }} alt="Logo" />
+          <h2 className="loginTitle">Crie sua conta</h2>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Usuário"
+                type="text"
+                variant="outlined"
+                color="secondary"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                sx={{ margin: '10px 0px' }}
+                fullWidth
+                label="E-mail"
+                type="email"
+                variant="outlined"
+                color="secondary"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+                <InputLabel color="secondary">Senha</InputLabel>
+                <OutlinedInput
+                  color="secondary"
+                  fullWidth
+                  label="Senha"
+                  type={visibilityToggle ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="Clique para exibir"
+                        onClick={toggleVisibility}
+                      >
+                        {visibilityToggle ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                sx={{ margin: '10px 0px' }}
+                fullWidth
+                label="Repetir Senha"
+                type={visibilityToggle ? 'text' : 'password'}
+                variant="outlined"
+                color="secondary"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="secondary"
+                endIcon={<LoginIcon />}
+              >
+                Registrar
+              </Button>
+            </Grid>
+            <Link
+              style={{
+                textAlign: 'center',
+                display: 'block',
+                width: '100%',
+                marginTop: '5px',
+                color: '#121F4E'
+              }}
+              to="/login"
+            >
+              Entrar
+            </Link>
+          </Grid>
+        </Stack>
+      </Grid>
+    </Grid>
+  )
 }
 
-export default Register;
+export default Register
